036470511bb64bd8c7c976dbdc7b698d37188df7
charlieli13 <licharlie13@gmail.com>
2024-05-28 15:01:46 -0700

    fixed bugs in stream and buffer types

--- src/argon/types/buffer.py
+++ src/argon/types/buffer.py
@@ -1,20 +1,28 @@
 from pydantic.dataclasses import dataclass
+from pydantic import ConfigDict
 from typing import Union, Tuple, override, List, TypeVar
 from argon.ref import Ref
+from argon.types.stream import gen_rank
 import numpy as np
+import numpy.typing as npt
 
-T = TypeVar("T", int, float, Tuple[int], Tuple[float])
-@dataclass
-class Element:
-    value: T
+T = TypeVar("T")
 
-    def __str__(self) -> str:
-        return str(self.value)
-
+@dataclass(config=ConfigDict(arbitrary_types_allowed=True))
+class Ndarray[T]:
+    value: np.ndarray[T]
+    
+    
 BT = TypeVar("BT")
+BRK = TypeVar("BRK")
 
-class Buffer[BT](Ref[np.ndarray, "Buffer[BT]"]):
+class Buffer[BT,BRK](Ref[Ndarray[BT], "Buffer[BT,BRK]"]):
 
     @override
-    def fresh(self) -> "Buffer[BT]":
-        return Buffer[self.BT]()
+    def fresh(self) -> "Buffer[BT,BRK]":
+        return Buffer[self.BT, self.BRK]()
+    
+    @override
+    def const(self, c: Ndarray[BT]) -> "Buffer[BT,BRK]":
+        assert type(gen_rank(c.value.shape)) == type(self.BRK)
+        return super().const(c)
--- src/argon/types/stream.py
+++ src/argon/types/stream.py
@@ -1,7 +1,6 @@
 from pydantic.dataclasses import dataclass
 from typing import Union, Tuple, override, List, TypeVar
 from argon.ref import Ref
-from argon.types.buffer import Element, Buffer
 
 @dataclass
 class Stop:
@@ -10,17 +9,22 @@ class Stop:
     def __str__(self) -> str:
         return f"S{self.level}"
 
-T = TypeVar("T", Element, Buffer, Tuple[Buffer])
+T = TypeVar("T")
 @dataclass
-class Val:
+class Val[T]:
     value: T
 
     def __str__(self) -> str:
         return str(self.value)
 
 VT = TypeVar("VT")
-class Stream[VT](Ref[List[Union[Val, Stop]], "Stream[VT]"]):
+RK = TypeVar("RK")
+class Stream[VT,RK](Ref[List[Union[Val[VT], Stop]], "Stream[VT,RK]"]):
 
     @override
-    def fresh(self) -> "Stream[VT]":
-        return Stream[self.VT]()
+    def fresh(self) -> "Stream[VT,RK]":
+        return Stream[self.VT, self.RK]()
+    
+def gen_rank(rank: int):
+    rank_name = 'R'+str(rank)
+    return type(rank_name, (), dict(rank=rank))
